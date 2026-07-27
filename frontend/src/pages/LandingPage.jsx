c80ec2ea99da11cba972c613bf8194a9890aa449
ravin-ship-it <xencomeon@gmail.com>
2026-07-28 05:27:29 +0530

    added click effect on Landing page buttons

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -9,8 +9,8 @@ const LandingPage = () => {
             <img src={logo} alt="Logo" className="h-96 w-full object-cover drop-shadow-[0_0_80px_#ff00bf80]" />
             <p className="px-28 text-4xl relative bottom-4 text-center text-cyan-100 leading-tight italic animate-pulse transition-all duration-300 ease-in-out">"Stop sending resumes. Start sharing your identity. A dynamic portfolio generator built for modern developers."</p>
             <div className="flex gap-8">
-                <Link to="/create" className="mt-16 mb-4 text-2xl px-8 py-4 font-semibold capitalize rounded-full text-white bg-blue-600 hover:bg-pink-600 hover:-translate-y-1 transition-all duration-300 ease-in-out">get started</Link >
-                <Link to="/profiles" className="mt-16 mb-4 text-2xl px-8 py-4 font-semibold capitalize rounded-full bg-white hover:bg-pink-600 hover:text-white hover:-translate-y-1 transition-all duration-300 ease-in-out">view profiles</Link >
+                <Link to="/create" className="mt-16 mb-4 text-2xl px-8 py-4 font-semibold capitalize rounded-full text-white bg-blue-600 hover:bg-pink-600 hover:-translate-y-1 hover:scale-110 active:scale-95 transition-all duration-300 ease-in-out">get started</Link >
+                <Link to="/profiles" className="mt-16 mb-4 text-2xl px-8 py-4 font-semibold capitalize rounded-full bg-white hover:bg-pink-600 hover:text-white hover:-translate-y-1 hover:scale-110 active:scale-95 transition-all duration-300 ease-in-out">view profiles</Link >
             </div>
         </div >
     )
