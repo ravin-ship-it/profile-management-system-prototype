4e812fb2a0a78d7686ec7a698000f5420f86ef7d
ravin-ship-it <xencomeon@gmail.com>
2026-07-28 15:57:20 +0530

    Added mobile and medium device responsiveness to All profile page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,12 +4,12 @@ import logo from "../assets/logo.png"
 const Header = () => {
     return (
         <header className="w-full flex items-center justify-between bg-black/90">
-            <Link to="/" className="h-24 w-60 block hover:scale-105 active:scale-95 overflow-clip transition-all duration-300 ease-in-out">
-                <img src={logo} alt="Profile Hub" className="w-full object-[0_-26px] hover:drop-shadow-[0_0_8px_#ff3399b3] transition-all duration-300 ease-in-out" />
+            <Link to="/" className="h-24 w-60 block relative right-4 md:right-0 hover:scale-105 active:scale-95 overflow-clip transition-transform">
+                <img src={logo} alt="Profile Hub" className="w-full object-[0_-26px] hover:drop-shadow-[0_0_8px_#ff3399b3] transition-transform" />
             </Link>
             <ul>
                 <li>
-                    <Link to="/create" className="mr-8 px-6 py-3 block text-2xl capitalize rounded-lg text-white/80 bg-pink-600 hover:scale-105 active:scale-95 transition-all duration-300 ease-in-out">create</Link>
+                    <Link to="/create" className="mr-4 md:mr-8 px-3 py-2 md:px-4 md:py-2 block md:text-2xl capitalize rounded-lg text-white/80 bg-pink-600 hover:scale-105 active:scale-95 transition-all duration-300 ease-in-out">create</Link>
                 </li>
             </ul>
         </header >
--- frontend/src/pages/AllProfilePage.jsx
+++ frontend/src/pages/AllProfilePage.jsx
@@ -46,16 +46,16 @@ const AllProfilePages = () => {
             </div>
 
 
-            <main className="p-8 w-full grid grid-cols-1 xl:grid-cols-3 md:grid-cols-2 gap-8 content-start">
+            <main className="p-4 md:p-8 w-full grid grid-cols-1 xl:grid-cols-3 md:grid-cols-2 gap-4 md:gap-8 content-start">
                 {profiles.map((profile) => (
 
                     <Link to={`/${profile._id}`} className="group block h-full w-full content-center border-[2px] border-solid border-black/50 rounded-2xl bg-[rgba(0,0,0,0.7)] hover:bg-[#002633bf] backdrop-blur-sm hover:scale-105 hover:text-shadow-[0_0_4px_cyan] hover:border-cyan-300 active:scale-95 transition-all duration-300 ease-in-out">
-                        <div key={profile._id} className="p-8 flex gap-4 items-center justify-between relative">
-                            <img src="https://picsum.photos/200" alt={`${profile.name}'s profile pic`} className="h-25 aspect-square object-center rounded-full border-2 border-cyan-500/80 group-hover:shadow-[0_0_10px_cyan] transition-all duration-300 ease-in-out" />
+                        <div key={profile._id} className="p-4 md:p-8 flex gap-4 items-center justify-between relative">
+                            <img src="https://picsum.photos/200" alt={`${profile.name}'s profile pic`} className="h-16 md:h-25 aspect-square object-center rounded-full border-2 border-cyan-500/80 group-hover:shadow-[0_0_10px_cyan] transition-all duration-300 ease-in-out" />
                             <div className="min-w-0 grow">
-                                <h2 className=" text-3xl grow truncate text-white/90">{profile.name}</h2>
-                                <p className=" text-2xl grow truncate text-white/70">{profile.email}</p>
-                                <p className="px-4 grow truncate absolute right-0 top-0 text-8xl text-white/30">{profile.age}</p>
+                                <h2 className="text-2xl md:text-3xl grow truncate text-white/90">{profile.name}</h2>
+                                <p className="md:text-2xl grow truncate text-white/70">{profile.email}</p>
+                                <p className="px-4 grow truncate absolute right-0 top-0 text-6xl md:text-8xl text-white/30">{profile.age}</p>
                             </div>
                         </div>
                     </Link>
