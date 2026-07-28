7bfb426505b42cb40bb8b2b1ed53a811e7aa00ee
ravin-ship-it <xencomeon@gmail.com>
2026-07-28 05:39:54 +0530

    small change xd

--- frontend/src/components/SocialMediaLinks.jsx
+++ frontend/src/components/SocialMediaLinks.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const SocialMediaLink = ({ svg, url }) => {
     return (
-        <Link to={url} className="h-20 w-20 active:scale-95 hover:scale-110 transition-all duration-300 ease-in-out bg-linear-30 rounded-xl from-pink-600 to-cyan-500">
+        <Link to={url} className="h-20 w-20 active:scale-95 hover:scale-110 transition-transform bg-linear-30 rounded-xl from-pink-600 to-cyan-500">
             {svg}
         </Link>
     )
