d86fe66de494042f37bc95900cc92e46bc565be0
ravin-ship-it <xencomeon@gmail.com>
2026-07-30 00:06:38 +0530

    adjusted loading screen

--- frontend/src/components/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner.jsx
@@ -1,7 +1,7 @@
 const LoadingSpinner = () => {
     return (
-        <div className="h-screen w-full flex flex-col gap-8 absolute z-50 inset-0 items-center justify-center bg-black shadow-[0_0_10px_cyan]">
-            <div className="h-32 w-32 flex items-center justify-center animate-bounce bg-transparent border-4 border-cyan-500 ">
+        <div className="h-screen w-full flex flex-col gap-4 absolute z-50 inset-0 items-center justify-center bg-black shadow-[0_0_10px_cyan]">
+            <div className="h-24 w-24 md:h-32 md:w-32 flex items-center justify-center animate-bounce bg-transparent border-4 border-cyan-500 ">
                 <div className="h-[50%] w-[50%] flex items-center justify-center animate-ping bg-transparent border-4 border-cyan-500 ">
                     <div className="h-[50%] w-[50%] flex items-center justify-center animate-ping bg-transparent border-4 border-cyan-500 ">
                         <div className="h-[50%] w-[50%] flex items-center justify-center animate-ping bg-transparent border-4 border-cyan-500 "></div>
